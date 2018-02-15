bf30764fd25275b3aba0aa3547eb2860cfe7b61b
Karin <koko@mailinator.com>
2018-02-15 09:31:39 +0200

    cdjchdj

---
+++ src/WaterHeater.java
@@ -0,0 +1,13 @@
+/**
+ * Created by hackeru on 15/02/2018.
+ */
+public class WaterHeater {
+    private static WaterHeater ourInstance = new WaterHeater();
+
+    public static WaterHeater getInstance() {
+        return ourInstance;
+    }
+
+    private WaterHeater() {
+    }
+}
